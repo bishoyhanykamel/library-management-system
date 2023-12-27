f93067d2f1f981a5718b26ce26d393d4021ca6f6
Bishoy Hany <bishoyhany31@hotmail.com>
2023-12-27 06:20:49 +0200

    Add GenericRepository

---
+++ LibraryMS.Core/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using LibraryMS.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMS.Core.Interfaces.Repositories
+{
+	public interface IGenericRepository<T> where T : BaseEntity
+	{
+		public Task<IEnumerable<T>> GetAllAsync();
+		public Task<T> GetByIdAsync(int id);
+		public Task<int> AddAsync(T item);
+		public Task<int> UpdateAsync(T item);
+		public Task<int> DeleteAsync(T item);
+	}
+}
---
+++ LibraryMS.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using LibraryMS.Core.Entities;
+using LibraryMS.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMS.Repository.Repositories
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		public Task<int> AddAsync(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> DeleteAsync(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<T>> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> UpdateAsync(T item)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
